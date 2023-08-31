e04de3a5d13cf1e288166c2f789ab07a4bcce499
vikyflores26 <136345512+vikyflores26@users.noreply.github.com>
2023-08-31 20:20:52 -0300

    tareaa

---
+++ clase1.js
@@ -0,0 +1,56 @@
+function potencia(x,a)
+{
+    let resultadofinal = (x * x)
+    return resultadofinal 
+}
+alert ( potencia (6 , 3))
+
+function cubo (a)
+{
+    let calcularalcubo = a,a,a  
+    return 'resultadocubo'+ calcularalcubo 
+
+}
+ alert (cubo(5))
+
+ function calcularproblemaA (x,a,b)
+ {
+    let resultado = (x - a)*b
+    return 'resultado del problemaA'+ resultadofinal
+ }
+ alert (calcularproblema ( 2,5,9))
+
+function calcularproblemaB (x,a,b)
+ {
+    let resultado = (x/a)+b
+    return 'el resultadodelproblemaB'+resultadofinal
+ }
+ alert (calcularproblemaA (7,6,8))
+
+ function calcularproblemaC (x,a,b)
+ {
+    let resultado = ( x a )b
+    return ' el resultadodelproblemaC'+ resultadofinal
+ }
+ alert (calcularproblemaC(9,8,7))
+
+ function calcularproblemaD ( x,a)
+ {
+    let resultado = (x x x ) + ( x * x ) + a 
+    return ' resultado del problema D '+ resultadofinal
+}
+alert( calcularproblemaD (4,5))
+
+function calcularproblemaE ( x,a,b)
+{
+    let resultadofinal = (x - a) * b 
+    return 'resultadoproblemaE'+ resultadofinal
+}
+alert(calcularproblemaE (5,8,10))
+
+function cuadrado (a)
+{
+    let calcularalcuadrado = b * b
+    return ' el resultado al cuadrado es'+ calcularalcuadrado
+}
+alert(cuadrado (9))
